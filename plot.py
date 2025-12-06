3f1edec57f03dc13b51ad8dbd041abeab6e2c458
Jinu_Kim <robotjinu25@skku.edu>
2025-12-07 01:43:31 +0900

    plot.py modified

--- plot.py
+++ plot.py
@@ -2,15 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def plot_data(df: pd.DataFrame, x_col: str, y_col: str, title: str = "Data Plot"):
-    plt.figure(figsize=(10, 6))
-    plt.plot(df[x_col], df[y_col], marker='o')
-    plt.title(title)
-    plt.xlabel(x_col)
-    plt.ylabel(y_col)
-    plt.grid(True)
+def plot_data(file_path, freq='day', value_col=None, start=None, end=None, step=1):
+    # 1) Load data
+    df = pd.read_excel(file_path)
+    df['date'] = pd.to_datetime(df['date'])
+    
+    # 2) Process data by frequency
+    if freq == 'year':
+        df_processed = (
+            df.sort_values('date')
+              .groupby(df['date'].dt.year)
+              .first()
+        )
+        df_processed.index.name = 'year'
+        df_processed.index = df_processed.index.astype(str)
+
+    elif freq == 'month':
+        df_processed = (
+            df.sort_values('date')
+              .groupby(df['date'].dt.to_period('M'))
+              .first()
+        )
+        df_processed.index = df_processed.index.astype(str)
+        df_processed.index.name = 'month'
+
+    elif freq == 'day':
+        df_processed = df.set_index('date')
+        df_processed.index = df_processed.index.astype(str)
+
+    # 3) start/end initialization
+    df_processed = df_processed.loc[start:end]
+
+    # 4) Plot
+    x = df_processed.index
+    y = df_processed[value_col]
+
+    plt.plot(x, y)
+    plt.xlabel(freq)
+    plt.xticks(x[::step], rotation=45, fontsize=10, fontstyle='italic')
+    plt.ylabel(value_col)
+    plt.title(f'<{value_col} over {freq}>', fontsize=20)
     plt.show()
 
-df = pd.read_excel('../data/010140/merged.xlsx')
-plot_data(df, x_col='date', y_col='PBR', title='PBR Over Time')
-plot_data(df, x_col='date', y_col='ROE', title='ROE Over Time')
+    return df_processed
+
+fpth = 'data/ECOS_day.xlsx'
+plot_data(fpth, freq='day', value_col='exchange_rate',
+          start='2025-01-01', end='2025-12-07', step=5)
